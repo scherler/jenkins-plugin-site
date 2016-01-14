fe232472bc3abdc440bcc911923a69e4abff236a
Thorsten Scherler <scherler@gmail.com>
2016-01-14 10:57:34 +0100

    [nodeInfrastructure] Bring back the old css

--- scripts/App.js
+++ scripts/App.js
@@ -4,7 +4,7 @@ import PluginsList from './plugins/view/PluginsList';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <h1>Jenkins Plugins List</h1>
         <PluginsList />
       </div>

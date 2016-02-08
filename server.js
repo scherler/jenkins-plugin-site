63820f56b0efe9a4e596559bc160971507e847a6
Thorsten Scherler <scherler@gmail.com>
2016-02-08 22:25:14 +0100

    [master] Making server a bit more inteligent, if jenkins update site is resolvable we are connecting there to get the source

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 /* eslint-disable no-console, no-var, strict */ //This is because of node being babel6 ready byet
-var express = require('express');
-var portServer = 3000;
-var portClient = 5000;
+const express = require('express');
+const dns = require('dns');
+const request = require('request');
+const portServer = 3000;
+const portClient = 5000;
 
 const content = `<ul>
     <li><a href="update-center.json">update-center.json</a></li>
     <li><a href="featured-service.json">featured-service.json</a></li>
   </ul>`;
 
-var app = express();
-app.use(express.static('server'));
+const app = express();
 // home page
 app.get('/', function(req, res) {
   res.send(content);
 });
-app.listen(portServer, function () {
-  console.log('Listening at port:', portServer);
+
+const host = 'updates.jenkins-ci.org';
+
+dns.resolve4(host, (err, addresses) => {
+  if (err) {
+    app.use(express.static('server'));
+    console.log('Using local plugin list');
+  } else {
+    app.use('/update-center.json', function(req, res) {
+      const url = 'https://' + host + '/current/update-center.json';
+      req.pipe(request(url)).pipe(res);
+    });
+  }
+  app.listen(portServer, function () {
+    console.log('Listening at port:', portServer);
+  });
 });
+
 
 var
   webpack = require('webpack'),

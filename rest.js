37ef5045e4674d5123659d1e4493d1cd3ca0c4b1
Thorsten Scherler <scherler@gmail.com>
2016-02-25 19:57:59 +0100

    [issue/WEBSITE-89] First working version of pagination, now to connect it with react

---
+++ rest.js
@@ -0,0 +1,127 @@
+/* eslint-disable no-console */ //This is because we are using console log for communications
+const
+  express = require('express'),
+  request = require('request'),
+  mongoose = require('mongoose'),
+  _ = require('lodash'),
+  mongoosePaginate = require('mongoose-paginate');
+
+const
+  Schema = mongoose.Schema,
+  rest = express(),
+  host = 'updates.jenkins-ci.org',
+  url = `https://${host}/current/update-center.json`,
+  backendPort = '3000',
+  connection = mongoose.connection;
+
+var Plugin;
+
+connection.on('error', console.error);
+connection.once('open', function() {
+  const pluginSchema = new Schema({
+    "buildDate": Date,
+    "dependencies": [],
+    "developers": [{
+      "developerId": String,
+      "email": String,
+      "name": String
+    }],
+    "excerpt": String,
+    "gav": String,
+    "labels": [String],
+    "name": String,
+    "_updated": {type: Date, default: Date.now},
+    "_created": {type: Date, default: Date.now},
+    "previousTimestamp": Date,
+    "previousVersion": String,
+    "releaseTimestamp": Date,
+    "requiredCore": String,
+    "scm": String,
+    "sha1": String,
+    "title": String,
+    "url": String,
+    "version": String,
+    "wiki": String
+  });
+
+  pluginSchema.plugin(mongoosePaginate);
+
+  Plugin = mongoose.model('Plugin', pluginSchema);
+});
+
+function getOptions(req) {
+  var page = req.query ? req.query.page : 1;
+  var limit = req.query ? req.query.limit : 10;
+  return options = {
+    lean:  true,
+    select: 'title name, version requiredCore wiki developers sha1 excerpt',
+    sort: { name: 1 },
+    page: page || 1,
+    limit: limit || 10
+  };
+}
+
+function search(query, req, callback) {
+  const options = getOptions(req);
+  Plugin.paginate({}, options, callback)
+}
+
+function getAll(req, callback) {
+  search({}, req, callback);
+}
+
+function setRestHeader(res) {
+  res
+    .header("Access-Control-Allow-Origin", "*")
+    .header("Access-Control-Allow-Headers", "X-Requested-With");
+}
+
+rest.get('/', (req, res) => {
+  res.send("<a href='/plugins'>Show plugins</a><a href='/indexDb'>Index db</a>");
+});
+
+/* FIXME:
+ * we need to decide what to do with this url (it is an update trigger).
+ * Either call it from within docker image with cron on regular basis,
+ * or via the React Client but with authorisation
+ */
+rest.get('/indexDb', (req, res) => {
+  // Disable caching
+  res
+    .header("Cache-Control", "no-cache, no-store, must-revalidate")
+    .header("Pragma", "no-cache")
+    .header("Expires", 0);
+
+  req
+    .pipe(request(`${url}?date=${Math.round(100000 * Math.random())}`, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+      if(body) {
+        var lines = body.split('\n');
+        if(lines.length >= 1){
+          var plugins = JSON.parse(lines[1]).plugins;
+          connection.db.dropCollection('plugins', (err, result)=> {
+            if (result) {
+              Plugin.create(_.values(plugins));
+            }
+          })
+        }
+      }
+    }
+    }))
+    .pipe(res);
+});
+
+rest.get('/plugins', (req, res) => {
+  setRestHeader(res);
+
+  getAll(req, (err, result) => {
+    res.json(result);
+  });
+});
+//FIXME: Adopt when moving to docker
+mongoose.connect('mongodb://localhost/plugins');
+rest.listen(backendPort, () => {
+  console.log('Listening backend port', backendPort);
+});
--- server.js
+++ server.js
@@ -2,35 +2,15 @@
 const express = require('express');
 const dns = require('dns');
 const request = require('request');
-const portServer = 3000;
+
+require('./rest');
+
 const portClient = 5000;
 
 const content = `<ul>
     <li><a href="update-center.json">update-center.json</a></li>
     <li><a href="featured-service.json">featured-service.json</a></li>
   </ul>`;
-
-const app = express();
-// home page
-app.get('/', (req, res) => res.send(content));
-
-const host = 'updates.jenkins-ci.org';
-
-dns.resolve4(host, (err, addresses) => {
-  if (err) {
-    app.use(express.static('server'));
-    console.log('Using local plugin list');
-  } else {
-    app.use('/update-center.json', (req, res) => {
-      const url = `https://${host}/current/update-center.json`;
-      req.pipe(request(url)).pipe(res);
-    });
-  }
-  app.listen(portServer, () => {
-    console.log('Listening at port:', portServer);
-  });
-});
-
 
 var
   webpack = require('webpack'),

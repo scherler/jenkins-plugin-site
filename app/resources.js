c2143b6bd1611b8385ad22a8f43bcd5b2275757d
Thorsten Scherler <scherler@gmail.com>
2016-03-01 18:13:22 +0100

    [issue/WEBSITE-89] fix typo

--- app/resources.js
+++ app/resources.js
@@ -103,8 +103,6 @@ export const actions = {
       .replace(' lugin','');
     type = type || '';
     const colors = ['#6D6B6D','#DCD9D8','#D33833','#335061','#81B0C4','#709aaa','#000'];
-    const color = colors[Math.floor(Math.random() * (colors.length - 1))];
-    const iconClass=`i ${type}  color${color}`;
     const color = colors[(title.length % 7)]; //pick color based on chars in the name to make semi-random, but fixed color per-plugin
     const iconClass=`i ${type};
     color = ${color}`;
